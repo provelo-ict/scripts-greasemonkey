bc886f55b1ce5ef2ee8a8143cbacd0607688a8d6
Pro Velo ICT <56344035+provelo-ict@users.noreply.github.com>
2020-09-22 13:29:33 +0200

    Launching only when using resource.activity model

--- FunkyTruck.js
+++ FunkyTruck.js
@@ -2,8 +2,10 @@
 //@namespace https://www.provelo.org/
 // @run-at context-menu
 // @name     FunkyTruckProVelo
-// @version  0.1
+// @version  0.1.1
+// @icon     https://www.provelo.org/favicon-32x32.png
 // @match    https://gestion.provelo.org/*
+// @match    https://provelo-test.coopiteasy.be/*
 // @grant    none
 // ==/UserScript==
 
@@ -12,18 +14,21 @@ function modifImages() {
     var champsBooleens = document.getElementsByClassName("oe_list_field_cell oe_list_field_boolean    ")
 
     var x = 0;
-    while (x < champsBooleens.length) {
-        if ((champsBooleens[x].attributes["data-field"].nodeValue == "need_delivery") && (champsBooleens[x].childNodes[0].checked == true)) {
-            champsBooleens[x].innerHTML = "<image src=\"https://signature.provelo.org/signature/images/camionnette.png\" height=12px>";
+    var fragmentid = window.location.hash.substr(1);
+    if (fragmentid.match("model=resource.activity")){
+        console.log("Loading truck images ⛟⛟ ");
+        while (x < champsBooleens.length) {
+            if ((champsBooleens[x].attributes["data-field"].nodeValue == "need_delivery") && (champsBooleens[x].childNodes[0].checked == true)) {
+                champsBooleens[x].innerHTML = "<image src=\"https://signature.provelo.org/signature/images/camionnette.png\" height=12px>";
+            }
+            x = x + 1;
         }
-        x = x + 1;
     }
    setTimeout(modifImages,3000); //Pour se relancer toutes les 3 secondes
-   console.log("Loading truck images ⛟⛟ ");
+
 };
-
-
 
 //setTimeout(modifImages,10000) //--> solution de base, on fait le travail une fois,  tout une fois - 10 seconde après le load
 
-modifImages();
+
+    modifImages();
